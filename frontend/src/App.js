f4ef627c183bc04bf6726646d4832a7752e2439e
Karim Zerman <whiskaswannacry@gmail.com>
2024-01-25 21:00:48 +0200

    User context has been created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Box, CssBaseline } from "@mui/material";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
+import { IsLoginedContext } from "./shared/Context";
 
 // Создание контекста для ColorMode
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
@@ -13,7 +14,7 @@ const ColorModeContext = createContext({ toggleColorMode: () => {} });
 function App() {
   // Хук навигации, нужен для клиентского роутинга
   // При вызове navigate первым параметром нужно передать путь в строковом формате, пример navigate("/home")
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // Тут все отвечает за создание состояния и функция смены темы
   const [mode, setMode] = React.useState("dark");
   const colorMode = React.useMemo(
@@ -37,43 +38,58 @@ function App() {
     [mode]
   );
 
+  // Стейт хранит boolean - вошёл юзер или нет
+  const [isLoginedUser, setIsLoginedUser] = React.useState(true);
+
   // Хук эффекта, реагирует на перерендер компонента App
   useEffect(() => {
-    // На данный момент его логика в том, чтобы при перезагрузке страницы пользователь оказывался на странице входа
-    // В дальнейшем нужно будет сделать фичу с сессией, и по ее истечению юзер попадает сюда
-    navigate('/auth')
-    
-    // Меняем название в title браузера на указанную строку
-    document.title = "Obd - вход в систему"
-  },[]);
+    if (!isLoginedUser) {
+      // На данный момент его логика в том, чтобы при перезагрузке страницы пользователь оказывался на странице входа
+      // В дальнейшем нужно будет сделать фичу с сессией, и по ее истечению юзер попадает сюда
+      navigate("/auth");
+
+      // Меняем название в title браузера на указанную строку
+      document.title = "Obd - вход в систему";
+    }
+  }, []);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline></CssBaseline>
-        <div className="outer-container">
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "100%",
-              maxWidth: "1600px",
-              bgcolor: "background.default",
-              color: "text.primary",
-              borderRadius: 1,
-              minHeight: "100%",
-            }}
-          >
-            <Header colorMode={colorMode} theme={theme}></Header>
-            <Routes>
-              <Route path="/auth" element={<Auth></Auth>}></Route>
-            </Routes>
-          </Box>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <IsLoginedContext.Provider value={{ isLoginedUser, setIsLoginedUser }}>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline></CssBaseline>
+          <div className="outer-container">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                maxWidth: "1600px",
+                // bgcolor: "background.default",
+                color: "text.primary",
+                borderRadius: 1,
+                minHeight: "100%",
+              }}
+            >
+              {isLoginedUser && (
+                <Header colorMode={colorMode} theme={theme}></Header>
+              )}
+              <Box
+                sx={{
+                  width: "100%",
+                }}
+              >
+                <Routes>
+                  <Route path="/auth" element={<Auth></Auth>}></Route>
+                </Routes>
+              </Box>
+            </Box>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </IsLoginedContext.Provider>
   );
 }
 
