e1ce4a01b82b67da2ed5ba83a5f9f39adda89d67
Karim Zerman <whiskaswannacry@gmail.com>
2024-01-29 17:34:18 +0200

    Color mode removed, new routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,36 +6,34 @@ import { Box, CssBaseline } from "@mui/material";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import { IsLoginedContext } from "./shared/Context";
+import Main from "./pages/Main/Main";
+import Tariffs from "./pages/Tariffs/Tariffs";
+import Services from "./pages/Services/Services";
+import Profile from "./pages/Profile/Profile";
+import Settings from "./pages/Settings/Settings";
+import styled from "@emotion/styled";
 
-// Создание контекста для ColorMode
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
+const InnerContainer = styled(Box)`
+  width: 100%;
+`;
+
+const RoutesContainer = styled(Box)`
+  width: 100%;
+`;
 
 // Основной компонент приложения
 function App() {
   // Хук навигации, нужен для клиентского роутинга
   // При вызове navigate первым параметром нужно передать путь в строковом формате, пример navigate("/home")
   const navigate = useNavigate();
-  // Тут все отвечает за создание состояния и функция смены темы
-  const [mode, setMode] = React.useState("dark");
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-        console.log(mode);
-      },
-    }),
-    []
-  );
 
-  // Создание самой палитры темы из MUI, реагирует на смену стейта mode
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
+  // Создание самой палитры темы из MUI
+  const theme = React.useMemo(() =>
+    createTheme({
+      palette: {
+        // mode: "light",
+      },
+    })
   );
 
   // Стейт хранит boolean - вошёл юзер или нет
@@ -49,17 +47,17 @@ function App() {
       navigate("/auth");
 
       // Меняем название в title браузера на указанную строку
-      document.title = "Obd - вход в систему";
+      document.title = "Вход в систему";
     }
   }, []);
 
   return (
     <IsLoginedContext.Provider value={{ isLoginedUser, setIsLoginedUser }}>
-      <ColorModeContext.Provider value={colorMode}>
+      {/* <ColorModeContext.Provider> */}
         <ThemeProvider theme={theme}>
           <CssBaseline></CssBaseline>
           <div className="outer-container">
-            <Box
+            <InnerContainer
               sx={{
                 display: "flex",
                 flexDirection: "column",
@@ -70,25 +68,29 @@ function App() {
                 // bgcolor: "background.default",
                 color: "text.primary",
                 borderRadius: 1,
-                minHeight: "100%",
               }}
             >
-              {isLoginedUser && (
-                <Header colorMode={colorMode} theme={theme}></Header>
-              )}
-              <Box
-                sx={{
-                  width: "100%",
-                }}
-              >
+              {isLoginedUser && <Header></Header>}
+              <RoutesContainer>
                 <Routes>
+                  <Route path="/" element={<Main theme={theme}></Main>}></Route>
                   <Route path="/auth" element={<Auth></Auth>}></Route>
+                  <Route path="/tariffs" element={<Tariffs></Tariffs>}></Route>
+                  <Route
+                    path="/services"
+                    element={<Services></Services>}
+                  ></Route>
+                  <Route path="/profile" element={<Profile></Profile>}></Route>
+                  <Route
+                    path="/settings"
+                    element={<Settings></Settings>}
+                  ></Route>
                 </Routes>
-              </Box>
-            </Box>
+              </RoutesContainer>
+            </InnerContainer>
           </div>
         </ThemeProvider>
-      </ColorModeContext.Provider>
+      {/* </ColorModeContext.Provider> */}
     </IsLoginedContext.Provider>
   );
 }
