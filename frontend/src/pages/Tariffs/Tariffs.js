0e1fa85fd6b918a7f2fcf2ef4888b2aff919d08e
Karim Zerman <whiskaswannacry@gmail.com>
2024-01-29 17:36:00 +0200

    The page created, some structure added

---
+++ frontend/src/pages/Tariffs/Tariffs.js
@@ -0,0 +1,167 @@
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import Grid from "@mui/material/Grid";
+import StarIcon from "@mui/icons-material/StarBorder";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import styled from "@emotion/styled";
+
+const TitleContainer = styled(Container)`
+
+`
+
+const PrimaryTitle = styled(Typography)`
+`
+
+const CardContainer = styled(Card)`
+
+`
+
+const UL = styled.ul`
+padding: 16px;
+`
+
+const tiers = [
+  {
+    title: "Free",
+    price: "0",
+    description: [
+      "10 users included",
+      "2 GB of storage",
+      "Help center access",
+      "Email support",
+    ],
+    buttonText: "Sign up for free",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "Pro",
+    subheader: "Most popular",
+    price: "15",
+    description: [
+      "20 users included",
+      "10 GB of storage",
+      "Help center access",
+      "Priority email support",
+    ],
+    buttonText: "Get started",
+    buttonVariant: "contained",
+  },
+  {
+    title: "Enterprise",
+    price: "30",
+    description: [
+      "50 users included",
+      "30 GB of storage",
+      "Help center access",
+      "Phone & email support",
+    ],
+    buttonText: "Contact us",
+    buttonVariant: "outlined",
+  },
+];
+
+const Tariffs = () => {
+  return (
+    <div>
+      {/* Hero unit */}
+      <TitleContainer
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6, }}
+      >
+        <PrimaryTitle
+          component="h1"
+          variant="h2"
+          align="center"
+          color="inherit"
+          gutterBottom
+        >
+          Тарифы
+        </PrimaryTitle>
+      </TitleContainer>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === "Enterprise" ? 12 : 6}
+              md={4}
+            >
+              <CardContainer>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: "center" }}
+                  action={tier.title === "Pro" ? <StarIcon /> : null}
+                  subheaderTypographyProps={{
+                    align: "center",
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === "light"
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "baseline",
+                      mb: 2,
+                    }}
+                  >
+                    <Typography
+                      component="h2"
+                      variant="h3"
+                      color="text.primary"
+                    >
+                      ${tier.price}
+                    </Typography>
+                    <Typography variant="h6" color="text.secondary">
+                      /mo
+                    </Typography>
+                  </Box>
+                  <UL>
+                    {tier.description.map((line) => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </UL>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth variant={tier.buttonVariant}>
+                    {tier.buttonText}
+                  </Button>
+                </CardActions>
+              </CardContainer>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default Tariffs;
