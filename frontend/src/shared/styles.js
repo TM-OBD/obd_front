eeedbb3ba22309b7b216b95ea19691dabf08e5ac
Karim Zerman <whiskaswannacry@gmail.com>
2024-02-06 13:00:32 +0200

    Exess comments have been removed

--- frontend/src/shared/styles.js
+++ frontend/src/shared/styles.js
@@ -1,17 +1,14 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
+  // Палитра цветов, пока использовать только primary, поскольку для secondary не определены нужные цвета
   palette: {
     primary: {
       main: '#F1F1F1',
-      // light: will be calculated from palette.primary.main,
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
     },
     secondary: {
       main: '#E0C2FF',
       light: '#F5EBFF',
-      // dark: will be calculated from palette.secondary.main,
       contrastText: '#47008F',
     },
   },
