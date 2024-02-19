74a2b2ae8a5703bdee8c26d75e63b7f78b3987d2
Karim Zerman <whiskaswannacry@gmail.com>
2024-02-19 14:30:02 +0200

    Bugs have fixed

--- frontend/src/components/Body/TitleBlock.js
+++ frontend/src/components/Body/TitleBlock.js
@@ -12,16 +12,23 @@ const Container = styled(Box)`
   position: relative;
   width: 100%;
   padding: 105px 7% 0 7%;
-  min-height: 100vh;
+`;
 
-  @media (max-width: 1000px) {
-    background-image: url(${Background});
-    background-size: cover;
-    background-position: center;
-  }
+const CarBackgroundImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
 `;
 
 const CarImage = styled.img`
+  position: absolute;
+  /* bottom: 0;
+  right: 0; */
+  top: 0;
+  right: 0; 
+  z-index: 3;
   width: 100%;
 
   @media (max-width: 1000px) {
@@ -38,128 +45,84 @@ const BackgroundImage = styled.img`
 `;
 
 const StyledButton = styled(Button)`
-
-  cursor: pointer;
-
   &:hover {
-    background-color: #0040FF !important;
+    background-color: #0040ff !important;
   }
-` 
+`;
 
 function TitleBlock() {
   return (
     <Container>
+      <CarBackgroundImg src={Background} alt="background"></CarBackgroundImg>
+      <CarImage src={Car} alt="car"></CarImage>
       <Box
         sx={{
-          position: "relative",
-          zIndex: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: { lg: "32px", md: "28px", sm: "20px", xs: "14px" },
         }}
       >
-        <Grid
-          container
-          flexDirection={"column"}
+        <Typography
+          fontWeight={"400"}
+          component="h1"
+          fontSize={{ lg: "180px", md: "120px", sm: "90px", xs: "60px" }}
           sx={{
-            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            color: (theme) => theme.palette.primary.main,
+            fontFamily: "'Unbounded', sans-serif",
+            zIndex: 2,
           }}
         >
-          <Grid item>
-            <Typography
-              fontWeight={"400"}
-              component="h1"
-				  fontSize={{ lg: "180px", md: "120px", sm: "90px", xs: "60px"}}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                width: "100%",
-                color: (theme) => theme.palette.primary.main,
-                fontFamily: "'Unbounded', sans-serif",
-              }}
-            >
-              ISyb Auto
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography
-              fontFamily={"Unbounded, sans-serif"}
-              fontWeight={"400"}
-				  fontSize={{ lg: "30px", md: "30px", sm: "25px", xs: "20px"}}
-              sx={{
-                color: (theme) => theme.palette.primary.main,
-              }}
-              display={"inline"}
-            >
-              сучасний сервіс
-              <span style={{ color: "#54AFEC" }}>	для авто</span>
-            </Typography>
-          </Grid>
-          <Grid item marginTop={{ lg: "70px", md: "50px", sm: "40px", xs: "30px"}}>
-            <Typography
-              fontFamily={"Inter, sans-serif"}
-              fontWeight={"400"}
-              fontSize={{ lg: "20px", md: "18px", sm: "16px", xs: "14px"}}
-              lineHeight={{ lg: "120%", sm: "110%",}}
-              sx={{
-                color: (theme) => theme.palette.primary.main,
-              }}
-              display={"inline"}
-            >
-              Встановивши спеціальне обладнання в <br />
-              OBD роз'єм, ви підключаєтеся до <br />
-              сервера, який починає моніторити ваш <br />
-              автомобіль у режимі
-              <span style={{ color: "#54AFEC", }}>	24/7.</span>
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            style={{
-				  marginTop: "30px",
-				  "@media (minWidth:600px)": {
-				  marginTop: "40px",
-				  },
-				  "@media (minWidth:900px)": {
-				  marginTop: "50px",
-				  },
-				  "@media (minWidth:1200px)": {
-				  marginTop: "70px",
-				  },
-            }}
-          >
-            <StyledButton
-              variant="contained"
-              sx={{
-                borderRadius: "15px",
-					 padding: { lg: "24px 50px", sm: "18px 30px", xs: "14px 20px"},
-                width: "335px",
-                height: "63px",
-                background: "#54AFEC",
-                fontFamily: "Inter, sans-serif",
-                fontWeight: "700",
-                fontSize: "18px",
-                color: "#f1f1f1",
-              }}
-            >
-              Придбати тариф
-            </StyledButton>
-          </Grid>
-        </Grid>
+          ISyb Auto
+        </Typography>
+        <Typography
+          fontFamily={"Unbounded, sans-serif"}
+          fontWeight={"400"}
+          fontSize={{ lg: "30px", md: "30px", sm: "25px", xs: "20px" }}
+          sx={{
+            color: (theme) => theme.palette.primary.main,
+            zIndex: 4,
+          }}
+        >
+          сучасний сервіс
+          <span style={{ color: "#54AFEC" }}> для авто</span>
+        </Typography>
+        <Typography
+          fontFamily={"Inter, sans-serif"}
+          fontWeight={"400"}
+          fontSize={{ lg: "20px", md: "18px", sm: "16px", xs: "14px" }}
+          lineHeight={{ lg: "120%", sm: "110%" }}
+          sx={{
+            color: (theme) => theme.palette.primary.main,
+            zIndex: 4,
+          }}
+        >
+          Встановивши спеціальне обладнання в <br />
+          OBD роз'єм, ви підключаєтеся до <br />
+          сервера, який починає моніторити ваш <br />
+          автомобіль у режимі
+          <span style={{ color: "#54AFEC" }}> 24/7.</span>
+        </Typography>
+        <StyledButton
+          variant="contained"
+          sx={{
+            cursor: "pointer",
+            borderRadius: { lg: "15px", md: "14px", sm: "10px", xs: "8px" },
+            width: { lg: "300px", md: "250px", sm: "150px", xs: "100px" },
+            height: { lg: "64px", md: "48px", sm: "32px", xs: "28px" },
+            background: "#54AFEC",
+            fontFamily: "Inter, sans-serif",
+            fontWeight: "700",
+            fontSize: { lg: "16px", md: "14px", sm: "10px", xs: "10px"},
+            color: "#f1f1f1",
+            zIndex: 4,
+          }}
+        >
+          Придбати тариф
+        </StyledButton>
       </Box>
-      <Grid container className="main-block__image _ibg">
-        <BackgroundImage
-          className="background-all"
-          src={Background}
-          alt="Background"
-          width={"100%"}
-          loading="lazy"
-        />
-			<CarImage
-          className="background-car"
-          src={Car}
-          alt="Background"
-          width={"100%"}
-          loading="lazy"
-        />
-      </Grid>
     </Container>
   );
 }
