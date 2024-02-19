c0b29c53e2ff24139c88ea102ca90baca8fe68db
Karim Zerman <whiskaswannacry@gmail.com>
2024-02-19 14:30:51 +0200

    Hrefs to sections objects have added

--- frontend/src/shared/Sections.js
+++ frontend/src/shared/Sections.js
@@ -1,8 +1,8 @@
 export const sectionsData = [
-    { id: 0, title: "Як це працює" },
-    { id: 1, title: "Тарифи" },
-    { id: 2, title: "Про нас" },
-    { id: 3, title: "Відгуки" },
-    { id: 4, title: "Контакти" },
-    { id: 5, title: "FAQ" },
+    { id: 0, title: "Як це працює", href: "#HIW", },
+    { id: 1, title: "Тарифи", href: "#Tariffs", },
+    { id: 2, title: "Про нас", href: "#AboutUs", },
+    { id: 3, title: "Відгуки", href: "#Reviews",},
+    { id: 4, title: "Контакти", href: "#Contacts", },
+    { id: 5, title: "FAQ", href: "#FAQ", },
 ];
