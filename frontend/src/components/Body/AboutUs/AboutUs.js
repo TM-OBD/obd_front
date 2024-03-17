5151dfce36b2b298c09a8808a453ab79d133af78
makstshkl <maxt2004@ukr.net>
2024-03-17 23:51:55 +0200

    fixed mistakes

--- frontend/src/components/Body/AboutUs/AboutUs.js
+++ frontend/src/components/Body/AboutUs/AboutUs.js
@@ -8,21 +8,15 @@ import MainImage from '../../../images/AboutUs/MainImage.png';
 import Bubbles from "../../../images/AboutUs/Bubble.png";
 
 
-const Container = styled(Box)``;
-
 const StyledGrid = styled(Grid)`
   @media(max-width: 900px){
     padding-left: 0px;
   }
 `;
 
-const StyledBgImg = styled(Box)``;
-
-
-
 function AboutUs() {
   return (
-    <Container 
+    <Box  //main container for about us block 
 	 id="AboutUs"
 	 sx={{
 		position: "relative",
@@ -39,17 +33,17 @@ function AboutUs() {
       <Typography
 		  variant="2"
 		  component="h2"
-		  width={'100%'}
-        fontFamily={"Unbounded, sans-serif"}
-        fontWeight={"400"}
-        fontSize={{ xl: '76px',lg: "60px", md: "60px", sm: '48px', xs: '36px'}}
         sx={{
           color: (theme) => theme.palette.primary.main,
+			 width: '100%',
+			 fontFamily: "Unbounded, sans-serif",
+			 fontWeight: '400',
+			 fontSize: { xl: '76px',lg: "60px", md: "60px", sm: '48px', xs: '36px'}
         }}
       >
         Про нас
       </Typography>
-		<StyledBgImg
+		<Box  //container for styled background image
         sx={{
           position: "absolute",
           width: {
@@ -77,18 +71,18 @@ function AboutUs() {
           backgroundPosition: "center",
           zIndex: 0,
         }}
-      ></StyledBgImg>
-      <Grid 
+      ></Box>
+      <Grid  //container for all texts
         container
-        paddingTop={{md: '50px', sm: '30px', xs: '30px'}}
         sx={{
+			paddingTop: {md: '50px', sm: '30px', xs: '30px'},
           display: 'flex',
           flexWrap: 'wrap',
           justifyContent: 'center',
         }}
       >
         {AboutUsData.map((item,index) => (
-          <StyledGrid 
+          <StyledGrid  //container for paragraph
             item
             key={item.id} 
             xs={12} 
@@ -104,11 +98,14 @@ function AboutUs() {
             }}
           >
             <Typography
-              fontFamily={'Inter, sans-serif'}
-              fontWeight={400}
-              fontSize={{ xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}}
-              lineHeight={'120%'}
-              color={(theme) => theme.palette.primary.main}
+				  key={index}
+				  sx={{
+					color: (theme) => theme.palette.primary.main,
+					fontFamily: "Inter, sans-serif",
+					fontWeight: '400',
+					lineHeight: '120%',
+					fontSize: { xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}
+				  }}
             >
               {item.firstParagraph &&
                 item.firstParagraph.split('ми завжди ставимо').map((part, index) => {
@@ -128,7 +125,7 @@ function AboutUs() {
                 })}
             </Typography>
             {index === 1 ? 
-              <Grid 
+              <Grid //container for image and text below img
                 item
                 sx={{
                   display: 'flex',
@@ -138,11 +135,14 @@ function AboutUs() {
               >
                 <img alt="MainImage" loading="lazy" src={MainImage} style={{ maxWidth: '100%', height: 'auto' }}/>
                 <Typography
-                  fontFamily={'Inter, sans-serif'}
-                  fontWeight={400}
-                  fontSize={{ xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}}
-                  lineHeight={'120%'}
-                  color={(theme) => theme.palette.primary.main}
+						key={index}
+						sx={{
+							color: (theme) => theme.palette.primary.main,
+							fontFamily: "Inter, sans-serif",
+							fontWeight: '400',
+							lineHeight: '120%',
+							fontSize: { xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}
+						}}
                 >
                   {item.paragraph &&
                     item.paragraph.split('Наша місія').map((part, index) => {
@@ -157,20 +157,26 @@ function AboutUs() {
               </Grid>
             : ''}
             <Typography
-              fontFamily={'Inter, sans-serif'}
-              fontWeight={400}
-              fontSize={{ xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}}
-              lineHeight={'120%'}
-              color={(theme) => theme.palette.primary.main}
+				  key={index}
+				  sx={{
+							color: (theme) => theme.palette.primary.main,
+							fontFamily: "Inter, sans-serif",
+							fontWeight: '400',
+							lineHeight: '120%',
+							fontSize: { xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}
+						}}
             >
               {item.secondParagraph}
             </Typography>
             <Typography
-              fontFamily={'Inter, sans-serif'}
-              fontWeight={400}
-              fontSize={{ xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}}
-              lineHeight={'120%'}
-              color={(theme) => theme.palette.primary.main}
+				key={index}
+				  sx={{
+							color: (theme) => theme.palette.primary.main,
+							fontFamily: "Inter, sans-serif",
+							fontWeight: '400',
+							lineHeight: '120%',
+							fontSize: { xl: "34px", lg: "20px", md: "20px", sm: '18px', xs: '16px'}
+						}}
             >
               {item.thirdParagraph &&
                 item.thirdParagraph.split('Ми прагнемо').map((part, index) => {
@@ -185,7 +191,7 @@ function AboutUs() {
           </StyledGrid>
         ))}
       </Grid>
-    </Container>
+    </Box>
   );
 }
 
