de483c803f09a986ae66082a64c7778ecf768e46
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-04-08 17:03:15 +0300

    [OS-118]: Fix arrows for reviews

--- src/components/Body/ReviewBlock/ReviewBlock.js
+++ src/components/Body/ReviewBlock/ReviewBlock.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import styled from "@emotion/styled";
-import { Navigation } from "swiper/modules";
+<<<<<<< Updated upstream
+import { A11y, Navigation, Pagination } from "swiper/modules";
+=======
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
+>>>>>>> Stashed changes
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
 import { ReviewsData } from "./ReviewsData";
-
-import starFilled from "../../../images/icons/star-filled.jpg";
-import starNotFilled from "../../../images/icons/star-notfilled.jpg";
+<<<<<<< Updated upstream
+import { Avatar } from "@mui/material";
+=======
+>>>>>>> Stashed changes
 
 const StyledSwiper = styled(Swiper)`
   width: 100%;
   height: 100%;
+  /* & .swiper-button-prev, .swiper-button-next {
+    top: auto;
+    bottom: 0;
+  } */
 `;
 
 const StyledSwiperSlide = styled(SwiperSlide)`
@@ -83,7 +92,7 @@ const RateSVG = styled("svg")`
   height: 14px;
 `;
 
-const UserReviewContiner = styled(Box)`
+const UserReviewContainer = styled(Box)`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -101,8 +110,10 @@ const UserReview = styled(Box)`
 `;
 
 const ReviewBlock = () => {
+  const swiperRef = useRef(null);
   return (
-    <Container id="Reviews"
+    <Container
+      id="Reviews"
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -110,26 +121,110 @@ const ReviewBlock = () => {
         gap: "50px",
         width: "100%",
         padding: "0 10% 0 10%",
+        // height: '70vh',
       }}
     >
-      <Typography
-        variant="h2"
-        component="h2"
+      <Box // Container for Title and buttons
         sx={{
-          fontWeight: 400,
-			 fontFamily: "Unbounded, sans-serif",
-			 zIndex: 2,
-          color: (theme) => theme.palette.primary.white.light,
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
         }}
       >
-        Відгуки
-      </Typography>
+        <Typography
+          variant="h2"
+          component="h2"
+          sx={{
+            fontWeight: 400,
+            fontFamily: "Unbounded, sans-serif",
+            zIndex: 2,
+            color: (theme) => theme.palette.primary.white.light,
+          }}
+        >
+          Відгуки
+        </Typography>
+        <Box // Container for arrow buttons (for PC)
+          sx={{
+            display: {
+              xl: "flex",
+              lg: "flex",
+              md: "flex",
+              sm: "none",
+              xs: "none",
+            },
+          }}
+        >
+          <Avatar
+            onClick={() => swiperRef.current.slidePrev()}
+            sx={{
+              cursor: "pointer",
+              backgroundColor: "transparent",
+            }}
+          >
+            <svg
+              width="50"
+              height="50"
+              viewBox="0 0 50 50"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M7.29166 24.9999C7.29166 24.4246 7.75803 23.9583 8.33332 23.9583H41.6667C42.242 23.9583 42.7083 24.4246 42.7083 24.9999C42.7083 25.5752 42.242 26.0416 41.6667 26.0416H8.33332C7.75803 26.0416 7.29166 25.5752 7.29166 24.9999Z"
+                fill="#54AFEC"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M21.5699 11.7633C21.9767 12.1701 21.9767 12.8297 21.5699 13.2365L9.80646 24.9999L21.5699 36.7633C21.9767 37.1701 21.9767 37.8297 21.5699 38.2365C21.1631 38.6433 20.5036 38.6433 20.0968 38.2365L7.59675 25.7365C7.18996 25.3297 7.18996 24.6701 7.59675 24.2634L20.0968 11.7633C20.5036 11.3566 21.1631 11.3566 21.5699 11.7633Z"
+                fill="#54AFEC"
+              />
+            </svg>
+          </Avatar>
+          <Avatar
+            onClick={() => swiperRef.current.slideNext()}
+            sx={{
+              cursor: "pointer",
+              backgroundColor: "transparent",
+            }}
+          >
+            <svg
+              width="50"
+              height="50"
+              viewBox="0 0 50 50"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M42.7083 24.9999C42.7083 24.4246 42.242 23.9583 41.6667 23.9583H8.33334C7.75805 23.9583 7.29168 24.4246 7.29168 24.9999C7.29168 25.5752 7.75805 26.0416 8.33334 26.0416H41.6667C42.242 26.0416 42.7083 25.5752 42.7083 24.9999Z"
+                fill="#54AFEC"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M28.4301 11.7633C28.0233 12.1701 28.0233 12.8297 28.4301 13.2365L40.1935 24.9999L28.4301 36.7633C28.0233 37.1701 28.0233 37.8297 28.4301 38.2365C28.8369 38.6433 29.4964 38.6433 29.9032 38.2365L42.4032 25.7365C42.81 25.3297 42.81 24.6701 42.4032 24.2634L29.9032 11.7633C29.4964 11.3566 28.8369 11.3566 28.4301 11.7633Z"
+                fill="#54AFEC"
+              />
+            </svg>
+          </Avatar>
+        </Box>
+      </Box>
       <StyledSwiper
+        modules={[Navigation, Pagination, A11y]}
         slidesPerView={1}
         spaceBetween={3}
-        navigation={{
-          clickable: true,
+<<<<<<< Updated upstream
+        navigation={false}
+=======
+        modules={[Pagination]}
+        pagination={{
+          dynamicBullets: true,
         }}
+        // scrollbar={{ draggable: true }}
+>>>>>>> Stashed changes
         breakpoints={{
           640: {
             slidesPerView: 2,
@@ -144,8 +239,8 @@ const ReviewBlock = () => {
             spaceBetween: 30,
           },
         }}
-        modules={[Navigation]}
         className="mySwiper"
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
         {ReviewsData.map((reviewData) => (
           <StyledSwiperSlide>
@@ -161,7 +256,7 @@ const ReviewBlock = () => {
                     sx={{
                       color: (theme) => theme.palette.primary.white.light,
                       fontSize: "12px",
-							 fontFamily: "Inter, sans-serif",
+                      fontFamily: "Inter, sans-serif",
                     }}
                   >
                     {reviewData.name}
@@ -172,7 +267,7 @@ const ReviewBlock = () => {
                     sx={{
                       color: (theme) => theme.palette.primary.blue.light,
                       fontSize: "12px",
-							 fontFamily: "Inter, sans-serif",
+                      fontFamily: "Inter, sans-serif",
                     }}
                   >
                     {reviewData.date}
@@ -211,9 +306,9 @@ const ReviewBlock = () => {
                 )}
               </UserRate>
             </SwiperHeader>
-            <UserReviewContiner>
+            <UserReviewContainer>
               <UserReview>{reviewData.review}</UserReview>
-            </UserReviewContiner>
+            </UserReviewContainer>
           </StyledSwiperSlide>
         ))}
       </StyledSwiper>
